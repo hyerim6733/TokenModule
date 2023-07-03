f0e4ac008991631c04b0eec16053a4a90fd84e0a
HR <hyerim6733@naver.com>
2023-07-03 18:09:15 +0900

    버전 확인

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,5 +11,20 @@ router.get('/login', function (req, res, next) {
 
   res.render('login');
 });
+
+router.get('/getVersion/dbdud', function (req, res, next) {
+  var cliver = req.query.version;
+  console.log(`서버버전 : `,cliver);
+  console.log(`유영버전 : `,process.env.dbdud);
+  if(cliver == process.env.dbdud) {
+    res.json({ result: "success" });
+  } else {
+    res.json({ result: "fail" });
+  }
+  
+  
+
+
+})
 
 module.exports = router;
--- routes/middletest.js
+++ routes/middletest.js
@@ -22,3 +22,23 @@ exports.act = (req, res, next) => {
     return next();
 
 }
+
+exports.cal = (req, res, next) => {
+    var a = req.query.a;
+    var b = req.query.b;
+    
+    try {
+        let c = a/b;
+        req.calcul = c;
+    } 
+    catch (error) {
+        console.log(error)
+        console.log(error.message)
+        return res.status(400).json({
+            code: 400,
+            message: '그냥.'
+        });
+    }
+    return next();
+
+}
